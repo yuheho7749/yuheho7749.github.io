ae506f242fa42bad6601bda9d46f0a4ac3165fd5
Joseph Ng <yuheho7749@gmail.com>
2024-07-20 17:34:45 -0700

    [Setup] Init project directories

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import AboutReact from './pages/AboutReact';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AboutReact />
   </React.StrictMode>
 );
 
--- src/pages/AboutReact.test.js
+++ src/pages/AboutReact.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AboutReact from './AboutReact';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<AboutReact />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
